b815fc618f31940619a90bf296472c0cc072ce4d
Bence Knáb <benceknab.iac@gmail.com>
2018-08-13 13:02:55 +0200

    Add development mode functionality to escrows
    The owner can set the contract to execute in production or development mode.
    In development mode the contract owner can set arbitrary dates for escrow
    expiration date to make testing easier.

---
+++ backend/contracts/EscrowHelper.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.24;
+
+import "./MarketHelper.sol";
+
+
+contract EscrowHelper is MarketHelper {
+
+  bool private isDevelopmentMode = true;
+  uint public conflictPeriod = 3 minutes;
+
+
+  modifier onlyDevelopmentMode() {
+    require(isDevelopmentMode, "This function can only be used in development mode.");
+    _;
+  }
+
+
+  function setDevelopmentMode() external onlyOwner {
+    isDevelopmentMode = true;
+    conflictPeriod = 3 minutes;
+  }
+
+  function setProductionMode() external onlyOwner {
+    isDevelopmentMode = false;
+    conflictPeriod = 3 days;
+  }
+
+}
