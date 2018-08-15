f16d5f321c2f6ec8f1b28c5b713c7c2b7497003e
Bence Knáb <benceknab.iac@gmail.com>
2018-08-15 15:51:09 +0200

    Refactor Sell component to fit container design pattern

--- frontend/src/components/Sell/Sell.jsx
+++ frontend/src/components/Sell/Sell.jsx
@@ -1,98 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
-import PropTypes from 'prop-types';
-import moment from 'moment';
 
-import { etherToWei } from '../../util/helpers';
-
-class Sell extends Component {
-  constructor(props, context) {
-    super(props);
-
-    this.contracts = context.drizzle.contracts;
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-
-    this.state = {
-      name: '',
-      price: 0,
-      guaranteedShippingTime: 0,
-    };
-  }
-
-  handleInputChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  handleSubmit() {
-    const { name, price, guaranteedShippingTime } = this.state;
-    const { Market } = this.contracts;
-
-    // TODO: prod mode
-    const gtsInSeconds = moment.duration(guaranteedShippingTime, 'minutes').asSeconds();
-    // const gtsInSeconds = moment.duration(guaranteedShippingTime, 'days').asSeconds();
-
-    Market.methods.createProductListing(
-      name,
-      etherToWei(price),
-      gtsInSeconds,
-    ).send();
-  }
-
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col
-            sm="12"
-            md={{ size: 8, offset: 2 }}
-          >
-            <Form>
-              <FormGroup>
-                <Label for="name">Name</Label>
-                <Input
-                  type="text"
-                  name="name"
-                  id="name"
-                  maxLength="60"
-                  placeholder="Name of the product"
-                  onChange={this.handleInputChange}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="price">Price</Label>
-                <Input
-                  type="number"
-                  name="price"
-                  id="price"
-                  placeholder="Price in ether"
-                  onChange={this.handleInputChange}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="guaranteedShippingTime">Guaranteed shipping time</Label>
-                <Input
-                  type="number"
-                  name="guaranteedShippingTime"
-                  id="guaranteedShippingTime"
-                  placeholder="Guaranteed shipping time"
-                  onChange={this.handleInputChange}
-                />
-              </FormGroup>
-              <Button onClick={this.handleSubmit}>Create Listing</Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
-
-Sell.contextTypes = {
-  drizzle: PropTypes.object,
-};
+const Sell = ({ handleInputChange, handleSubmit }) => (
+  <Container>
+    <Row>
+      <Col
+        sm="12"
+        md={{ size: 8, offset: 2 }}
+      >
+        <Form>
+          <FormGroup>
+            <Label for="name">Name</Label>
+            <Input
+              type="text"
+              name="name"
+              id="name"
+              maxLength="60"
+              placeholder="Name of the product"
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="price">Price</Label>
+            <Input
+              type="number"
+              name="price"
+              id="price"
+              placeholder="Price in ether"
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="guaranteedShippingTime">Guaranteed shipping time</Label>
+            <Input
+              type="number"
+              name="guaranteedShippingTime"
+              id="guaranteedShippingTime"
+              placeholder="Guaranteed shipping time"
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+          <Button onClick={handleSubmit}>Create Listing</Button>
+        </Form>
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default Sell;
---
+++ frontend/src/components/Sell/SellContainer.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { drizzleConnect } from 'drizzle-react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+import Sell from './Sell';
+import { etherToWei } from '../../util/helpers';
+
+class SellContainer extends Component {
+  constructor(props, context) {
+    super(props);
+
+    this.contracts = context.drizzle.contracts;
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      name: '',
+      price: 0,
+      guaranteedShippingTime: 0,
+    };
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit() {
+    const { name, price, guaranteedShippingTime } = this.state;
+    const { Market } = this.contracts;
+
+    // TODO: prod mode
+    const gtsInSeconds = moment.duration(guaranteedShippingTime, 'minutes').asSeconds();
+    // const gtsInSeconds = moment.duration(guaranteedShippingTime, 'days').asSeconds();
+
+    Market.methods.createProductListing(
+      name,
+      etherToWei(price),
+      gtsInSeconds,
+    ).send();
+  }
+
+  render() {
+    return (
+      <Sell
+        handleInputChange={this.handleInputChange}
+        handleSubmit={this.handleSubmit}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  accounts: state.accounts,
+  drizzleStatus: state.drizzleStatus,
+});
+
+SellContainer.contextTypes = {
+  drizzle: PropTypes.object,
+};
+
+export default drizzleConnect(SellContainer, mapStateToProps);
